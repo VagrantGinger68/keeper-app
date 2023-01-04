3a66cae18a403de56dacd6d85db9ae778d813ccd
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-01-04 17:04:18 -0500

    changed the app to allow new notes to be created and deleted

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,44 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+// import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 function App(){
+
+    const [notes, setNotes] = useState([]);
+
+    function addNote(newNote){
+        setNotes(prevNotes => {
+           return [...prevNotes, newNote]
+        })
+    }
+
+    function deleteNote(id){
+        setNotes(prevNotes => {
+            return prevNotes.filter((noteItem, index)=>{
+                return index !== id;
+            })
+        })
+    }
+
     return (
     <Fragment>
         <Header />
         <Footer />
-        {notes.map( note => 
-        <Note 
-            key={note.key}
-            noteTitle={note.title}
-            noteBody={note.content}    
-        />   
-        )}
+        <CreateArea 
+        onAdd={addNote}
+        />
+        {notes.map((noteItem, index)=>{
+            return (<Note 
+                key={index}
+                id={index}
+                title={noteItem.title}
+                content={noteItem.content}
+                onDelete={deleteNote}
+            />)
+        })}
     </Fragment>
 )};
 
